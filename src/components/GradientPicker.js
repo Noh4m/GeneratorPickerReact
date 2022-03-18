8dea59029b95dc68d01c85de63e7efb71e3a2e41
Noham <72859612+Noh4m@users.noreply.github.com>
2022-03-18 12:20:16 +0100

    first commit

---
+++ src/components/GradientPicker.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+const GradientPicker = () => {
+    const [color, setColor] = useState()
+    const [color2, setColor2] = useState()
+
+    const handleChangeColor = e => {
+        setColor( e.target.value)
+    }
+
+    const handleChangeColor2 = e => {
+        setColor2( e.target.value)
+    }
+
+    return (
+        <>
+            <div className="d-flex flex-row ">
+                <div className="card w-50 m-5 ">
+                    <div className="card-body">
+                        <input type="color" className="w-100"  onChange={handleChangeColor}/>
+                    </div>
+                </div>
+                    <div className="card w-50 m-5 ">
+                        <div className="card-body">
+                            <input type="color" className="w-100"  onChange={handleChangeColor2}/>
+                        </div>
+                    </div>
+            </div>
+
+            <div className="h-50 w-10" style={{ backgroundImage: `linear-gradient(to right,`+ color + `,`+ color2 + ` )` }}>
+ds
+            </div>
+        </>
+
+
+    );
+};
+
+export default GradientPicker;
