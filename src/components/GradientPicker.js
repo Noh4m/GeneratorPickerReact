c465d890956493a2b46465fe2f803f73601ac84e
Noham <72859612+Noh4m@users.noreply.github.com>
2022-03-18 14:10:28 +0100

    Add breakPoint gradient

--- src/components/GradientPicker.js
+++ src/components/GradientPicker.js
@@ -3,9 +3,20 @@ import React, {useState} from 'react';
 const GradientPicker = () => {
     const [color, setColor] = useState()
     const [color2, setColor2] = useState()
+    const [colorBp, setColorBp] = useState()
+    const [color2Bp, setColor2Bp] = useState()
+    const test = `linear-gradient(to right,`+ color+ " " + colorBp+`%` +`,` + color2 + " " + color2Bp+`%` +` )`
 
     const handleChangeColor = e => {
         setColor( e.target.value)
+    }
+    const handleChangeColorBp = e => {
+        setColorBp(e.target.value)
+        console.log(colorBp)
+    }
+    const handleChangeColor2Bp = e => {
+        setColor2Bp(e.target.value)
+        console.log(color2Bp)
     }
 
     const handleChangeColor2 = e => {
@@ -18,18 +29,23 @@ const GradientPicker = () => {
                 <div className="card w-50 m-5 ">
                     <div className="card-body">
                         <input type="color" className="w-100"  onChange={handleChangeColor}/>
+                        <input type="range" className="w-100"  onChange={handleChangeColorBp}/>
                     </div>
                 </div>
                     <div className="card w-50 m-5 ">
                         <div className="card-body">
                             <input type="color" className="w-100"  onChange={handleChangeColor2}/>
+                            <input type="range" className="w-100"  onChange={handleChangeColor2Bp}/>
                         </div>
                     </div>
             </div>
 
-            <div className=" border border-dark w-10" style={{ backgroundImage: `linear-gradient(to right,`+ color + `,`+ color2 + ` )`, height: "300px" }}>
+            <div className=" border border-dark w-10" style={{ backgroundImage: test , height: "300px" }}>
 
             </div>
+
+            <p> {test}</p>
+
         </>
 
 
