7578882d6d6fbba4888f388ad2784cf589056e87
Noham <72859612+Noh4m@users.noreply.github.com>
2022-03-18 15:19:38 +0100

    Rotation deg

--- src/components/GradientPicker.js
+++ src/components/GradientPicker.js
@@ -3,48 +3,88 @@ import React, {useState} from 'react';
 const GradientPicker = () => {
     const [color, setColor] = useState()
     const [color2, setColor2] = useState()
+    const [color3, setColor3] = useState()
     const [colorBp, setColorBp] = useState()
     const [color2Bp, setColor2Bp] = useState()
-    const test = `linear-gradient(to right,`+ color+ " " + colorBp+`%` +`,` + color2 + " " + color2Bp+`%` +` )`
+    const [color3Bp, setColor3Bp] = useState()
+    const [ratotion, setRatotion] = useState()
+    const test = `linear-gradient(`+ratotion+`deg`+`,`+ color+ " " + colorBp+`%` +`,` + color2 + " " + color2Bp+`%` +`,`+ color3 + " " + color3Bp+`%` + ` )`
 
     const handleChangeColor = e => {
         setColor( e.target.value)
-    }
-    const handleChangeColorBp = e => {
-        setColorBp(e.target.value)
-        console.log(colorBp)
-    }
-    const handleChangeColor2Bp = e => {
-        setColor2Bp(e.target.value)
-        console.log(color2Bp)
     }
 
     const handleChangeColor2 = e => {
         setColor2( e.target.value)
     }
+    const handleSetRatotion = e => {
+        setRatotion( e.target.value)
+    }
+    const handleChangeColor3 = e => {
+        setColor3( e.target.value)
+    }
+
+    const handleChangeColorBp = e => {
+        setColorBp(e.target.value)
+
+    }
+
+    const handleChangeColor2Bp = e => {
+        setColor2Bp(e.target.value)
+    }
+
+    const handleChangeColor3Bp = e => {
+        setColor3Bp(e.target.value)
+    }
+
+
+
 
     return (
         <>
-            <div className="d-flex flex-row ">
-                <div className="card w-50 m-5 ">
-                    <div className="card-body">
-                        <input type="color" className="w-100"  onChange={handleChangeColor}/>
-                        <input type="range" className="w-100"  onChange={handleChangeColorBp}/>
+            <div className="row row-cols-2">
+                <div className="col">
+                    <div className="card m-2">
+                        <div className="card-body">
+                            <input type="color" className="w-100"  onChange={handleChangeColor}/>
+                            <p>BreakPoint 1</p>
+                            <input type="range" className="w-100"  onChange={handleChangeColorBp}/>
+                        </div>
                     </div>
                 </div>
-                    <div className="card w-50 m-5 ">
+                <div className="col">
+                    <div className="card m-2">
                         <div className="card-body">
                             <input type="color" className="w-100"  onChange={handleChangeColor2}/>
+                            <p>BreakPoint 2</p>
                             <input type="range" className="w-100"  onChange={handleChangeColor2Bp}/>
                         </div>
                     </div>
+                </div>
+                <div className="col">
+                    <div className="card m-2">
+                        <div className="card-body">
+                            <input type="color" className="w-100"  onChange={handleChangeColor3}/>
+                            <p>BreakPoint 3</p>
+                            <input type="range" className="w-100"  onChange={handleChangeColor3Bp}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card m-2">
+                        <div className="card-body">
+                            <p>Rotation</p>
+                            <input type="range" className="w-100" min="0" max="360"   onChange={handleSetRatotion}/>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className=" border border-dark w-10" style={{ backgroundImage: test , height: "300px" }}>
 
             </div>
 
-            <p> {test}</p>
+            <p> Background-color: {test}</p>
 
         </>
 
